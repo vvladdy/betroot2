4efe848fb279f52dbbc48fb4d975110b49f6585c
vvladdy <yablonvl@icloud.com>
2022-07-23 17:08:24 +0300

    Lesson 16

--- lesson 16.py
+++ lesson 16.py
@@ -4,7 +4,7 @@ print('Task 1')
 # School
 # Make a class structure in python representing people at school. Make a
 # base class called Person, a class called Student, and another one called
-# Teacher. Try to find as many methods and attributes as you can which belong
+# Teacher. Try to find as many methods and attributes as you can, which belong
 # to different classes, and keep in mind which are common and which are not.
 # For example, the name should be a Person attribute, while salary should
 # only  be available to the teacher.
@@ -32,6 +32,7 @@ class Person:
     def gender_woman(self):
         print(self.gender[1])
 
+
 class Student(Person):
     def __init__(self, firstname, secondname, age):
         super().__init__(firstname, secondname)
@@ -39,14 +40,14 @@ class Student(Person):
 
     def agest(self):
         if 7 <= self.age < 18:
-            print (f'{self.firstname} {self.secondname} школьник {self.age} '
-                   f'лет')
+            print(f'{self.firstname} {self.secondname} школьник {self.age} '
+                  f'лет')
 
     def go_to_school(self):
-        print (f'{self.firstname} {self.secondname} ходит в школу учиться')
+        print(f'{self.firstname} {self.secondname} ходит в школу учиться')
 
     def do_homework(self):
-        print (f'{self.firstname} {self.secondname} делает домашние задания')
+        print(f'{self.firstname} {self.secondname} делает домашние задания')
 
 
 class Teacher(Person):
@@ -56,13 +57,13 @@ class Teacher(Person):
 
     def agest(self):
         if 18 <= self.age < 78:
-            print (f'{self.firstname} {self.secondname} учитель {self.age} лет')
+            print(f'{self.firstname} {self.secondname} учитель {self.age} лет')
 
     def go_to_school(self):
         print(f'{self.firstname} {self.secondname} ходит в школу работать')
 
     def do_homework(self):
-        print (f'{self.firstname} {self.secondname} проверяет домашние задания')
+        print(f'{self.firstname} {self.secondname} проверяет домашние задания')
 
     def salary(self):
         print(f'{self.firstname} {self.secondname} получает зарплату')
@@ -85,8 +86,9 @@ def main():
 if __name__ == '__main__':
     main()
 
-
 print('\nTask 2')
+
+
 # Mathematician
 #
 # Implement a class Mathematician which is a helper class for doing math
@@ -102,7 +104,7 @@ print('\nTask 2')
 class Mathematician:
 
     def square_nums(self, a):
-        outlist = list(map(lambda x: x**2, a))
+        outlist = list(map(lambda x: x ** 2, a))
         return outlist
 
     def remove_positives(self, a):
@@ -127,8 +129,9 @@ def main():
 if __name__ == '__main__':
     main()
 
-
 print('\nTask 3')
+
+
 # Product Store
 #
 # Write a class Product that has three attributes:
@@ -160,66 +163,99 @@ print('\nTask 3')
 
 
 class Product:
-    def __init__(self, type: str, name, price):
-        self.type = type
+    def __init__(self, type_: str, name, price):
+        self.type = type_
         self.name = name
         self.price = price
+        self.amount = None
 
     def info(self):
         print(f'Type product: {self.type}\nName: {self.name.split()}\nPrice'
               f': {self.price}')
 
-class ProductStore(Product):
 
-    def __init__(self, type, name, price):
-        super().__init__(type, name, price)
+class ProductStore:
+    def __init__(self, *args: Product):
+        self.income = 0
+        self.all_prod = []
+        for product in args:
+            self.prod = product
+            self.prod.amount = 1
+            self.all_prod.append(product)
 
-    def add(self, amount):
-        self.price = self.price + self.price*amount/100
-
-    def set_discount(self, identifier, percent):
-        if identifier in self.name:
-            print(f'скидка на товар {identifier}: '
-                  f'{(self.price * percent) / 100} денег')
-            self.price = self.price - self.price*percent/100
+    def add_product(self, product, amount, price_premium = 0.3):
+        if product in self.all_prod:
+            product.amount += amount
         else:
-            print(f'товарa {identifier}: нет в магазине')
-            self.price = self.price
+            self.all_prod.append(product)
+            product.price *= (1 + price_premium)
+            product.amount = amount
 
-    def add_product(self, prod, price):
-        if prod not in self.name:
-            self.name += ' ' + prod
-            self.price = self.price + self.price * price / 100
-        print(self.name.split(), self.price)
+    def set_discount(self, identifier, percent, identifier_type = 'name'):
+        if identifier_type == 'name':
+            for prod in self.all_prod:
+                if prod.name == identifier:
+                    prod.price *= (1 - percent / 100)
+        elif identifier_type == 'type':
+            for prod in self.all_prod:
+                if prod.type == identifier:
+                    prod.price *= (1 - percent / 100)
+
+    def sell_product(self, product_name, amount):
+        for prod in self.all_prod:
+            if prod.name == product_name and prod.amount >= amount:
+                prod.amount -= amount
+                self.income += prod.price * amount
+
+    def get_income(self):
+        return self.income
+
+    def get_all_products(self):
+        for prod in self.all_prod:
+            print(prod.name, prod.type, prod.price, prod.amount)
+
+    def get_prod_info(self, product_name):
+        print([prod.__dict__ for prod in self.all_prod if prod.name ==
+               product_name])
+        # check = ''
+        # for prod in self.all_prod:
+        #     if prod.name == product_name:
+        #         check += 'something'
+        #         return prod.name, prod.amount
+        # if not check:
+        #     raise ValueError('Such prod doe not exist')
 
 
-
-p = Product('Sport', 'Football T-Shirt', 100)
+p1 = Product('Sport', 'Football T-Shirt', 100)
 p2 = Product('Food', 'Ramen', 1.5)
-s = ProductStore('Sport', 'Football T-Shirt', 100)
-s.add_product('Lola', 12)
-s.info()
+p3 = Product('Sport', 'Skirts', 1)
 
-#s.add()
-# s.add(p2, 300)
-#s.sell(‘Ramen’, 10)
-#assert s.get_product_info(‘Ramen’) == (‘Ramen’, 290)
+print(p1.info())
+s = ProductStore()
+s.add_product(p1, 10)
+s.add_product(p2, 300)
+
+s.sell_product('Ramen', 10)
+print(s.get_all_products())
+
+assert s.get_prod_info('Ramen') == ('Ramen', 290)
 
 
+print('\nTask 4')
 
-
-print('\n Task 4')
 # Create your custom exception named `CustomException`, you can inherit from
 # base Exception class, but extend its functionality to log every error
 # message to a file named `logs.txt`. Tips: Use __init__ method to extend
 # functionality for saving messages to file
 
-# ```
-#
-# class CustomException(Exception):
-#
-# def __init__(self, msg):
-#
-# ...
-#
-# ```
+
+class CustomException(Exception):
+
+    def __init__(self, msg):
+        super().__init__(msg)
+        with open('logs.txt', 'a') as file:
+            file.write(msg + '\n')
+
+
+raise CustomException('Hello world!!!')
+
